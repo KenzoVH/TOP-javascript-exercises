15c84c686f1e7ceffbb1ec9609959ea3c038eb6d
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-17 12:15:08 -0400

    Finished exercises 01 through 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = "";
+    if (num < 0) {
+        returnString += "ERROR";
+    } else {
+        for (i = 1; i <= num; i++) {
+            returnString += string;
+        }
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    stringArray = stringArray.reverse();
+    let output = stringArray.join();
+    output = output.replace(/,/g, "")
+    return output;
 };
 
 // Do not edit below this line
