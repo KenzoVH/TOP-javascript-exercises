cc639e0a493392e85eaa173c3e51c7419695e77c
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-26 10:21:49 -0400

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.reduce((oldestPerson, person) => {
+        const currentAge = calculateAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        return oldestAge < currentAge ? person : oldestPerson;
+    });
+    return oldest;
 };
+
+const calculateAge = function(birthYear, deathYear) {
+    if(!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
